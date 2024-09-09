3d5c7d2712729f50988b3b20c7b92d0c89dd7550
Yudayana Arif Prasojo <yuda.arif.prasojo@gmail.com>
2024-09-09 16:12:56 +0700

    db: create and migrate TropicalPlant model

--- main/models.py
+++ main/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class TropicalPlant(models.Model):
+  name = models.CharField(max_length=255)
+  price = models.IntegerField()
+  description = models.TextField()
+  weight = models.FloatField()
+  created_at = models.DateTimeField(auto_now_add=True)
+  
+  @property
+  def is_expensive(self):
+    return self.price > 100
