5101fd8a15730a1d54e50609a569886c7f95bf48
Yudayana Arif Prasojo <yuda.arif.prasojo@gmail.com>
2024-09-09 16:23:06 +0700

    feat: add routing and context passing to template

--- main/views.py
+++ main/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
 
 # Create your views here.
+def show_main(request):
+  context = {
+    'app_name': 'Plantify Shop',
+    'name': 'Yudayana Arif Prasojo',
+    'class': 'PBP-D',
+    'npm': '2306215160'
+  }
+  
+  return render(request, "main.html", context)
