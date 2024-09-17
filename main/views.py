ea2a84208b041a871fc07f6b7f2f8e1e2c66873b
Yudayana Arif Prasojo <yuda.arif.prasojo@gmail.com>
2024-09-17 23:04:16 +0700

    add: dynamic templates

--- main/views.py
+++ main/views.py
@@ -1,14 +1,18 @@
 from django.shortcuts import redirect, render
 
+
 # Create your views here.
 def show_main(request):
-  context = {
-    'app_name': 'Plantify Shop',
-    'name': 'Yudayana Arif Prasojo',
-    'class': 'PBP-D',
-        'npm': '2306215160'}
-  
-  return render(request, "main.html", context)
+
+    context = {
+        "app_name": "Plantify Shop",
+        "name": "Yudayana Arif Prasojo",
+        "class": "PBP-D",
+        "npm": "2306215160",
+    }
+
+    return render(request, "main.html", context)
+
 
 def redirect_home(request):
-    return redirect('home/')
+    return redirect("home/")
