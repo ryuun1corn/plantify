3921185c5b38ca4e32cf239817eb9094d48bf3e6
Yudayana Arif Prasojo <yuda.arif.prasojo@gmail.com>
2024-09-17 23:15:45 +0700

    refactor: add id field and migrate

--- main/models.py
+++ main/models.py
@@ -1,8 +1,11 @@
+import uuid
+
 from django.db import models
 
 
 # Create your models here.
 class TropicalPlant(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     name = models.CharField(max_length=255)
     price = models.IntegerField()
     description = models.TextField()
