134deda2e72bbdb6a6a099e11746398ac9dda6e5
Yudayana Arif Prasojo <yuda.arif.prasojo@gmail.com>
2024-09-17 19:39:04 +0700

    add: git actions and host in settings

--- plantify/settings.py
+++ plantify/settings.py
@@ -19,13 +19,16 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
+# SECURITY WARNING - keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-bn1@#m%m$v0#v^sr$lalv8ef=41(l%zgjty2$9=0zt&*2y0*uc'
 
-# SECURITY WARNING: don't run with debug turned on in production!
+# SECURITY WARNING - don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["localhost", "127.0.0.1", "yudayana-arif-plantify.pbp.cs.ui.ac.id"]
+ALLOWED_HOSTS = ["localhost", "127.0.0.1", 
+                 "yudayana-arif-plantify.pbp.cs.ui.ac.id",
+                 "http://yudayana-arif-plantify.pbp.cs.ui.ac.id",
+                 "https://yudayana-arif-plantify.pbp.cs.ui.ac.id"]
 
 
 # Application definition
