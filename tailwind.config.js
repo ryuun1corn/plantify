3507c180293ba7fa93e6910f67da791c1c038ddf
Yudayana Arif Prasojo <yuda.arif.prasojo@gmail.com>
2024-09-30 09:13:16 +0700

    chore: add tailwind config file for lsp support

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+const config = {
+  content: [
+    "./templates/**/*.html",
+    "./your_app_name/**/*.py", // Replace with your actual app name
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
+
+export default config;
