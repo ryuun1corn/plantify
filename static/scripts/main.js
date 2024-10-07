7718710caf90086e027b6d996703a9783f247db9
Yudayana Arif Prasojo <yuda.arif.prasojo@gmail.com>
2024-10-07 15:55:05 +0700

    feat: implement show modal to add plants

--- static/scripts/main.js
+++ static/scripts/main.js
@@ -61,4 +61,34 @@ async function refreshTropicalPlants() {
   document.getElementById("tropical_plants_cards").className = classNameString;
   document.getElementById("tropical_plants_cards").innerHTML = htmlString;
 }
+
 refreshTropicalPlants();
+
+const modal = document.getElementById("crudModal");
+const modalContent = document.getElementById("crudModalContent");
+
+function showModal() {
+  const modal = document.getElementById("crudModal");
+  const modalContent = document.getElementById("crudModalContent");
+
+  modal.classList.remove("hidden");
+  setTimeout(() => {
+    modalContent.classList.remove("opacity-0", "scale-95");
+    modalContent.classList.add("opacity-100", "scale-100");
+  }, 50);
+}
+
+function hideModal() {
+  const modal = document.getElementById("crudModal");
+  const modalContent = document.getElementById("crudModalContent");
+
+  modalContent.classList.remove("opacity-100", "scale-100");
+  modalContent.classList.add("opacity-0", "scale-95");
+
+  setTimeout(() => {
+    modal.classList.add("hidden");
+  }, 150);
+}
+
+document.getElementById("cancelButton").addEventListener("click", hideModal);
+document.getElementById("closeModalBtn").addEventListener("click", hideModal);
